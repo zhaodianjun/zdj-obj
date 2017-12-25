af9154f6ecf018fda48003aa037cd8bf942bda38
zhaodianjun <a909002881@163.com>
2017-12-25 16:16:04 +0800

    message

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+  color:orange;
+  font-size:76px;
+}
+</style>
